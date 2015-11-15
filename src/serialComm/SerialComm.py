0623239122ffd85e5855a53e8e8812b1a06cd09f
jrhoeber <jordan.hoeber@gmail.com>
2015-11-15 11:11:27 -0500

    STUFF WORKS

--- src/serialComm/SerialComm.py
+++ src/serialComm/SerialComm.py
@@ -1,16 +1,19 @@
 import serial
 from string import *
+import time
 class SerialComm:
     def __init__(self):
         self.arduinoSerial = serial.Serial("/dev/cu.usbmodemFD121", baudrate=9600)
+        time.sleep(2)
 
     def moveToPosition(self, opos, npos):
         s = str(opos[0]),str(opos[1]),str(npos[0]),str(npos[1]) , "\n"
-        print self.arduinoSerial.isOpen()
         self.arduinoSerial.write(s[0])
         self.arduinoSerial.write(s[1])
         self.arduinoSerial.write(s[2])
         self.arduinoSerial.write(s[3])
+    
+    def closeConnection(self):
         self.arduinoSerial.close()
 
 s = SerialComm()
